3c154409244b7db081ea67b31e9d75503418f48d
May <ycliu@jyu.edu.cn>
2017-12-02 22:35:56 +0800

    sdfsdfs

---
+++ stuMgm/src/test/GradeTest.java
@@ -0,0 +1,141 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.jyu.stumgm.entity.Grade;
+
+public class GradeTest {
+
+	Grade g = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		g = new Grade();
+	
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+//	@Test
+	public void testGetId() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGrade() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGradeStudentIntIntIntIntIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetStudent() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetStudent() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumElectron() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumElectron() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumSoftware() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumSoftware() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumSecurity() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumSecurity() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumJava() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumJava() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumDB() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumDB() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumImage() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumImage() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumDistributed() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumDistributed() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetNumTest() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testSetNumTest() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+
+}
